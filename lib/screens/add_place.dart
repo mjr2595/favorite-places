69ff770593af38e169e69e4e687b1e2e5e00e8bf
mjr2595 <michaeljohnraymond@gmail.com>
2024-01-01 17:17:51 -0600

    Add ImageInput widget to AddPlaceScreen

--- lib/screens/add_place.dart
+++ lib/screens/add_place.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import 'package:favorite_places/widgets/image_input.dart';
 import 'package:favorite_places/providers/user_places.dart';
 
 class AddPlaceScreen extends ConsumerStatefulWidget {
@@ -45,6 +46,8 @@ class _AddPlaceScreenState extends ConsumerState<AddPlaceScreen> {
                 color: Theme.of(context).colorScheme.onBackground,
               ),
             ),
+            const SizedBox(height: 16),
+            ImageInput(),
             const SizedBox(height: 16),
             ElevatedButton.icon(
               onPressed: _savePlace,
